9e461cd98f0d7902471009e14fdd4a3e9ed28917
vaibhavarora1205 <vaibhavarora1205@gmail.com>
2026-08-03 10:26:15 +0530

    Loops

---
+++ javascript/loops.js
@@ -0,0 +1,19 @@
+// let x=5;
+// let f=1;
+// for(let i=1;i<=x;i++)
+// {
+//     f=f*i;
+// }
+// console.log(`Factorial of ${x} is ${f}`);
+
+// let a=0;
+// let b=1;
+// for(let i=0;i<5;i++)
+// {
+//     let next=a+b;
+//     a=b;
+//     b=next;
+//     console.log(next);
+// }
+
+// patterns
