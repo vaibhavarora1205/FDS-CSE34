fd85514308ad2f974a14a02b24c05dbc1652c76f
vaibhavarora1205 <vaibhavarora1205@gmail.com>
2026-08-03 10:03:34 +0530

    Control Flow statement

---
+++ javascript/SelectionDecisionCtrl.js
@@ -0,0 +1,105 @@
+// let year=Number(prompt("Enter a Year: "));
+// let year=1100;
+// if(year%100==0)
+// {
+//     if(year%400==0)
+//     {
+//     console.log(`${year} is a leap year`);
+//     }
+//     else
+//     {
+//         console.log(year,"Not a leap year");
+//     }
+// }
+// else
+// {
+//     if(year%4==0)
+//     {
+//         console.log("Leap Year");
+//     }
+//     else
+//     {
+//         console.log("Not a leap year");
+//     }
+// }
+
+// if((year%100!=0 && year%4==0)|| year%400==0)
+// {
+//     console.log(year,"Leap Year");
+// }
+// else
+// {
+//     console.log(year,"Not a leap Year");
+// // }
+// let year=1100;
+// (year%100!=0)?((year%4==0)?console.log("Leap Year"):console.log("not leap")):((year%400==0)?console.log("Leap Year"):console.log("Not a leap Year"));
+// ((year%100!=0 && year%4==0)|| year%400==0)?console.log("leap year"):console.log("Not a leap Year");
+
+
+let x=1;
+// switch(x)
+// {
+//     case 1:
+//         console.log("1");
+//         break;
+//     case 2:
+//         console.log("2");
+//         break;
+//     case 3: 
+//         console.log("3");
+//         break;
+//     default:
+//         console.log("Default");
+// }
+
+// let day=Number(prompt("Enter day Number(1 to 7)"));
+// let day=3;
+// switch(day)
+// {
+//     case 1:
+//         console.log("Monday");
+//         break;
+//     case 2:
+//         console.log("Tuesday");
+//         break;
+//     case 3:
+//         console.log("Wednesday");
+//         break;
+//     case 4:
+//         console.log("Thursday");
+//         break;
+//     case 5:
+//         console.log("Friday");
+//         break;
+//     case 6:
+//         console.log("Saturday");
+//         break;
+//     case 7:
+//         console.log("Sunday");
+//         break;
+//     default:
+//         console.log("Enter valid day number");
+// }
+
+
+
+let a=10;
+let b=20;
+let op="/";
+switch(op)
+{
+    case "+":
+        console.log(a+b);
+        break;
+    case "-":
+        console.log(a-b);
+        break;
+    case "*":
+        console.log(a*b);
+        break;
+    case "/":
+        console.log(a/b);
+        break;
+    default:
+        console.log("Enter valid operator");
+}
