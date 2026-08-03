7ae7096fb5360428706cd33decc5e5572cfeebab
vaibhavarora1205 <vaibhavarora1205@gmail.com>
2026-08-03 10:03:05 +0530

    day 2

---
+++ javascript/swap.js
@@ -0,0 +1,14 @@
+let a;
+let b;
+a=10;
+b=20;
+console.log(`Entered numbers are: ${a},${b}`);
+a=a+b;
+b=a-b;
+a=a-b;
+console.log(`Swapped numbers are: ${a},${b}`);
+
+// let a=100,b=20,c;
+// if(a<15)
+//     c=30;
+// console.log(a,b,c);
