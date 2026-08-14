bef49f73f80c0c8abc32dc74d1a975b443b7977f
vaibhavarora1205 <vaibhavarora1205@gmail.com>
2026-08-14 11:30:14 +0530

    Loops

--- javascript/loops.js
+++ javascript/loops.js
@@ -17,3 +17,70 @@
 // }
 
 // patterns
+// i=0;
+// for(; ;i=i+2)
+// {
+//     if(i>10)
+//         {
+//             break;
+//         }
+//         console.log(i);
+// }
+
+// Sum of digit of 5 digit number
+// reverse
+// prime
+// fibinacci
+
+// let num=12345
+// let sum=0;
+// let a;
+// while(num>0)
+// {
+//     a=num%10;
+//     sum+=a;
+//     num=num/10;
+// }
+// console.log(Math.floor(sum))
+
+
+// // reverse
+// let num=12345
+// let rev=0;
+// let a;
+// while(num>0)
+// {
+//     a=num%10;
+//     rev=rev*10+a
+//     num=num/10;
+// }
+// console.log(rev)
+
+// let a=0;
+// let b=1;
+// console.log(a)
+// console.log(b)
+// for(let i=0;i<5;i++)
+// {
+//     let next=a+b;
+//     a=b;
+//     b=next;
+//     console.log(next);
+// }
+
+// prime
+// let num=13;
+// for(let i=2;i<num/2;i++)
+// {
+//     if(num%i==0)
+//     {
+//         console.log("Not prime");
+//         break;
+//     }
+// }
+// console.log("Prime");
+
+
+let sum=0;
+for(let i=0;i<=9;i++, sum+=i)
+console.log(sum);
